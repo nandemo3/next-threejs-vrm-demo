2bcfc4071578e2327125177914f9ddc5594cb6e8
naoto.horie <horie.naoto@synamon.jp>
2023-06-02 10:22:39 +0900

    show VRM model

---
+++ src/app/Model.tsx
@@ -0,0 +1,49 @@
+import { FC, useState, useEffect } from "react"
+import { Html} from "@react-three/drei"
+import { GLTFLoader, GLTF } from "three/examples/jsm/loaders/GLTFLoader"
+import { VRMLoaderPlugin } from "@pixiv/three-vrm"
+
+
+const Model: FC = () => {
+  const [gltf, setGltf] = useState<GLTF>()
+  const [progress, setProgress] = useState<number>(0)
+
+  useEffect(() => {
+    if (!gltf) {
+      const loader = new GLTFLoader()
+      loader.register((parser) => {
+        return new VRMLoaderPlugin(parser)
+      })
+
+      loader.load(
+        "/models/AliciaSolid.vrm",
+        (tmpGltf) => {
+          setGltf(tmpGltf)
+          console.log("loaded")
+        },
+        // called as loading progresses
+        (xhr) => {
+          setProgress((xhr.loaded / xhr.total) * 100)
+          console.log((xhr.loaded / xhr.total) * 100 + "% loaded")
+        },
+        // called when loading has errors
+        (error) => {
+          console.log("An error happened")
+          console.log(error)
+        }
+      )
+    }
+  }, [gltf])
+
+  return (
+    <>
+      {gltf ? (
+        <primitive object={gltf.scene} />
+      ) : (
+        <Html center>{progress} % loaded</Html>
+      )}
+    </>
+  )
+}
+
+export default Model
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,36 @@
+"use client"
+
+import React, { useRef } from 'react'
+import { Canvas } from '@react-three/fiber'
+
+import { OrbitControls } from "@react-three/drei"
+import Model from './Model'
+
 export default function Home() {
+  const gltfCanvasParentRef = useRef<HTMLDivElement>(null)
+
   return (
     <main>
+      <div
+        ref={gltfCanvasParentRef}
+        style={{ height: 1000 }}
+      >
+        <Canvas
+          frameloop="demand"
+          camera={{ fov: 20, near: 0.1, far: 300, position: [0, 1, -10] }}
+          flat
+        >
+          <directionalLight position={[1, 1, -1]} color={"0xFFFFFF"} />
+          <Model />
+          <color attach="background" args={["#f7f7f7"]} />
+          <OrbitControls
+            enableZoom={false}
+            enablePan={false}
+            enableDamping={false}
+          />
+          <gridHelper />
+        </Canvas>
+      </div>
     </main>
   )
 }
